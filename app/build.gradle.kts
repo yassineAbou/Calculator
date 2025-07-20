fc48f1643772eebec3fb103a1b9ec6ca080fcebe
yassineabou <aboyassine555@gmail.com>
2025-07-20 01:09:43 +0100

    Make app to be compilable with android 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("androidx.navigation.safeargs")
     id("com.google.devtools.ksp")
-
 }
 
 @Suppress("UnstableApiUsage")
@@ -16,15 +15,14 @@ android {
         dataBinding = true
     }
 
-    compileSdk = 33
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.yassineabou.calculator"
         minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.1"
-
+        targetSdk = 36
+        versionCode = 6
+        versionName = "1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -59,22 +57,22 @@ java {
 dependencies {
 
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.core:core-ktx:1.10.1")
-    val navVersion = "2.6.0"
-    val roomVersion = "2.5.2"
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.core:core-ktx:1.16.0")
+    val navVersion = "2.9.2"
+    val roomVersion = "2.7.2"
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     // sdp
-    implementation("com.intuit.sdp:sdp-android:1.1.0")
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
     // ssp
-    implementation("com.intuit.ssp:ssp-android:1.1.0")
+    implementation("com.intuit.ssp:ssp-android:1.1.1")
     // mXparser
-    implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
+    implementation("org.mariuszgromada.math:MathParser.org-mXparser:6.1.0")
     // navigation
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
@@ -82,8 +80,8 @@ dependencies {
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
     // hilt
-    implementation("com.google.dagger:hilt-android:2.51.1")
-    kapt("com.google.dagger:hilt-compiler:2.51.1")
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    kapt("com.google.dagger:hilt-compiler:2.56.2")
 }
 android {
     buildTypes {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val kotlinVersion by rootProject.extra { "1.9.22" }
+    val kotlinVersion by rootProject.extra { "2.2.0" }
 
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-        val navVersion = "2.6.0"
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.56.2")
+        val navVersion = "2.9.2"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
@@ -15,11 +15,11 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.android.test") version "8.2.2" apply false
-    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+    id("com.android.application") version "8.11.1" apply false
+    id("com.android.library") version "8.11.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    id("com.android.test") version "8.11.1" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 
 
 }
